f3fc8e6c6a363e7795fd0ae38c76b164a36004ea
Olimshoh <154997975+olimshoh@users.noreply.github.com>
2026-03-23 21:51:52 +0300

    Update Student.cs

--- Student.cs
+++ Student.cs
@@ -1,1 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace DiplomaApp
+{
+    public struct Student
+    {
+        public string LastName;
+        public string FirstName;
+        public string Group;
+        public int Grade;
+    }
+}
